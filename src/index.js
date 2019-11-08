238221434fa1d96fb4986fe01bf6ea5d1c9c8a33
Arleigh <80908258@qq.com>
2019-11-08 17:53:31 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import index from './css/index.css';
+
+
+var sub = require('./sub');
+var app = document.createElement('div');
+app.innerHTML = '<h1>Hello World h1</h1>';
+app.appendChild(sub());
+document.body.appendChild(app);
+
+
+console.log(12341234112, 'adsfasdf');
---
+++ src/sub.js
@@ -0,0 +1,9 @@
+
+//我们这里使用CommonJS的风格
+function generateText() {
+    var element = document.createElement('h2');
+    element.id = "twoH"
+    element.innerHTML = "Hello h2 world h2";
+    return element;
+}
+module.exports = generateText;
